9fbbf95df42e393e14481ff959b3385550cee1d0
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-08-01 21:36:03 +0300

    first commit

---
+++ 4.8.1/4.8.1.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool compare(int a, int b) {
+    return a < b;
+}
+void sort(std::vector<int>& v) {
+    std::sort(v.begin(), v.end(), compare);
+    auto it = std::unique(v.begin(), v.end());
+    v.erase(it, v.end());
+}
+void print(std::vector <int> v) {
+    for (auto& it : v) {
+        std::cout << it << " ";
+    }
+    std::cout << std::endl;
+}
+int main()
+{
+    std::cout << "[IN] : ";
+    std::vector <int> v{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    print(v);
+    sort(v);
+    std::cout << "[OUT] : ";
+    print(v);   
+}
---
+++ 4.8.2/4.8.2.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <string>
+#include <set>
+
+template <class T>
+void print_container(std::set <T> set_) {
+    auto it = set_.begin();
+    for (it; it != set_.end(); ) {
+        std::cout << *it << " ";
+        std::advance(it, 1);
+    }
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+}
+
